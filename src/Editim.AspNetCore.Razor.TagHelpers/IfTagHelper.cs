3c421bdaa13d03282c6cb673a9bc885070bcb5a0
editim <editim@users.noreply.github.com>
2017-10-16 17:58:45 +0300

    Init project

---
+++ src/Editim.AspNetCore.Razor.TagHelpers/IfTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Editim.AspNetCore.Razor.TagHelpers
+{
+    [HtmlTargetElement(Attributes = "asp-if")]
+    public class IfTagHelper : TagHelper
+    {
+        [HtmlAttributeName("asp-if")]
+        public bool Condition { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (!Condition)
+            {
+                output.SuppressOutput();
+            }
+        }
+    }
+}
